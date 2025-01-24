7344a01800070871c724386250ae29f2e15f0de3
gionandez <gio.nandez23@gmail.com>
2025-01-24 18:30:12 -0500

    Se agrega array

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// let amigo = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if(amigo != ''){
+        console.log("El campo no esta vacio");
+        limpiarCaja();
+    }else{
+        alert("El campo esta vacio");
+        limpiarCaja();
+    }
+    
+}
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = "";
+}
